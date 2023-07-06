09a2c60d9d011cc22db46fbb69c7b717def9bd1b
Vishal-Bysani <125885053+Vishal-Bysani@users.noreply.github.com>
2023-07-06 18:23:30 +0530

    Uploaded_my_script.sh

--- Section-1/Challenges/challenge-2/script.sh
+++ Section-1/Challenges/challenge-2/script.sh
@@ -1,3 +1,28 @@
-# flag{REDACTED}
+# flag{flag{93tt1n9_u536_t0_6a5h} }
 
-echo "ech0"
+#!/bin/bash
+i=1
+url="https://www.cse.iitb.ac.in/~akshatka/"
+while [ 0 ]
+    do
+        wget $url
+
+        file=`ls | grep *.html`
+
+        url=`cat $file | grep -o "https://[A-Za-z0-9].*html"`
+
+        if [ -z $url ]
+            then
+            url=`cat $file | grep -o "https://[A-Za-z0-9].*tar.gz"`
+            wget $url 
+            rm $file
+            file=`ls | grep *.tar.gz`
+            break
+        fi
+
+        rm $file
+    done
+
+tar -xvf $file
+
+flag=`cat `find . -type f -size 87369c`|grep -o "flag{.*}"`
